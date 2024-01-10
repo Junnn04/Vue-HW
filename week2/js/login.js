75dd52abab26ab9eb184b8a2ed0b4eac1cd9d802
Jun <wd1995vo421@gmail.com>
2024-01-10 20:03:11 +0800

    新增W2 login

---
+++ week2/js/login.js
@@ -0,0 +1,28 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  data(){
+    return{
+       user:{
+        user:'',
+        password:'',
+       },
+    }
+  },
+  methods:{
+    login(){
+      const url: 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios.post(url,this.user)
+      .then((res)=>{
+        const {token,expired} = res.data;
+        //寫入cookie
+        document.cookie = `hextoken=${token};expired=${new Data(expired)}`;
+        window.location = 'products.html';
+      })
+       .catch((err)=>{
+        alert(err.res.data.message)
+       });
+    },
+    
+  },
+}).mount("#app");
