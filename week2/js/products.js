64ba21df58d991375685e0027c09c2006a990496
Jun <wd1995vo421@gmail.com>
2024-01-10 20:43:19 +0800

    新增 產品頁面

---
+++ week2/js/products.js
@@ -0,0 +1,44 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+// 產品資料格式
+createApp({
+  data() {
+    return {
+      url:'https://vue3-course-api.hexschool.io',
+      apiPath:'junapi',
+      products:[],
+      tempProduct:{},
+     }
+  },
+  methods:{
+    checkAdmin(){
+      const apiUrl = `${this.url}/v2/api/user/check`;
+      axios.post(apiUrl)
+      .then(()=>{
+        this.getData();
+      })
+      .catch((err)=>{
+        alert(err.response.data.message)
+        window.location = `login.html`;
+      })
+     },
+     getData(){
+      const apiUrl = `${this.url}/v2/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+      .then((res)=>{
+         this.products = res.data.products;    
+      })
+      .catch((err)=>{
+        alert(err.res.data.message)
+      })
+     },
+     productDetail(item){
+      this.tempProduct = item;
+     }
+  },
+  mounted(){
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin()
+  }
+}).mount("#app");
